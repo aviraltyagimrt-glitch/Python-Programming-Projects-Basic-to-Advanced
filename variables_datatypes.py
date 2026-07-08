deecf49096c80f31c446cbe4d695e608e937b50b
Aviral Tyagi <aviraltyagi.mrt@gmail.com>
2026-07-08 21:25:17 +0530

    Variable and Datatypes done

---
+++ variables_datatypes.py
@@ -0,0 +1,24 @@
+a = "Aviral"
+print(a)
+b = 1
+print(b)
+c = True
+print(c)
+d = None
+print(d)
+e = complex(8,27)
+print(e)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
+L = [8,27,"Aviral","Tyagi","!~@,.#$",[45,"What's up",8],"It's an example of list"]
+print(L)
+print(type(L))
+T = (27,8,"Friend",(25,"Bye","@##./32,","354"),526,"done")
+print(T)
+print(type(T))
+D = {"Name":"Aviral","Surname":"Tyagi","DOB":"27 June 2009","Mulank":9}
+print(D)
+print(type(D))
