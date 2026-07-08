626cb44716bbdda385a1737a09b3ad438f1768e4
Aviral Tyagi <aviraltyagi.mrt@gmail.com>
2026-07-08 16:51:03 +0530

    cmt and escape sequences done

---
+++ comments_escape.sequences.py
@@ -0,0 +1,5 @@
+print("Hello everyone today I am going to learn python\nand will make some projects afterwards!!\n\"they can be 3/4\" ")
+#This is a single line comment
+'''This is a multiline comment'''
+"""It's also an example of multiline comment"""
+print("Aviral",2,7,sep="!~",end="at")
