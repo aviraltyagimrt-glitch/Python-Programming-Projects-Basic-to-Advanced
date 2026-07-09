d56c38273413535c053a71e3cedd4cec598fd399
Aviral Tyagi <aviraltyagi.mrt@gmail.com>
2026-07-09 13:30:47 +0530

    Taking Inputs done

---
+++ Taking_Inputs.py
@@ -0,0 +1,9 @@
+a = input("Enter your First Name : \n")
+b = input("Enter your Last Name : \n")
+print(f"Your full nane is : {a+' '+b}")#or for space I can use {a} {b} with space 
+x = input("Enter the First Number :\n")
+y = input("Enter the Second Number :\n")
+print(f"The sum of {x} and {y} is : {int(x)+int(y)}")
+y1 = int(input("Enter the Second Number :\n"))
+y2 = float(input("Enter the Second Number :\n"))
+print(f"The sum of {y1} and {y2} is {y1 + y2}")
