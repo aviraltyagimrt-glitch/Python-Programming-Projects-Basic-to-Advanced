ee05b3fd79785082f8eb4bda869e6fef05cb24ae
Aviral Tyagi <aviraltyagi.mrt@gmail.com>
2026-07-09 12:42:34 +0530

    Typecasting done

---
+++ Implicit_and_Explicit_Typecasting.py
@@ -0,0 +1,10 @@
+#Implicit Type Casting
+x = 5
+y = 8.675
+print(f"The sum of x and y is : {x+y}")
+a = "8"
+b = "27"
+print(f"The sum of a and b is {int(a)+float(b)}")
+z1 = 56
+z2 = 67.67
+print(f"The sum of z1 and z1 is {float(z1)+int(z2)}")
