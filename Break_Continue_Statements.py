d6445e4bbccb6ace5d09a580bc1df1d5fc38f3b2
Aviral Tyagi <aviraltyagi.mrt@gmail.com>
2026-07-11 17:58:32 +0530

    Break And Continue Statements Done

---
+++ Break_Continue_Statements.py
@@ -0,0 +1,9 @@
+for i in range (18):
+    if (i == 10):
+        continue # This will skip the interation on which continue is implemented . 
+    print(i)
+print()
+for i in range (20):
+    if (i == 10):
+        break # This will break the entire loop .
+    print(i)
