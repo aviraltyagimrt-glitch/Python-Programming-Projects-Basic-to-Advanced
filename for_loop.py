52c3fc7b08c7fa84d9f9ab78fa1d67948333cead
Aviral Tyagi <aviraltyagi.mrt@gmail.com>
2026-07-11 13:17:46 +0530

    For loop Done

--- for_loop.py
+++ for_loop.py
@@ -1,3 +1,15 @@
 a = "Aviral"
 for i in a:
-    print(i,end="")
+    print(i,end="\n")
+List = ["Aviral","Tyagi","College","Let's see"]
+for i in List:
+    print(i)
+    for j in i:
+        print(j,end = " ")
+print()
+for n in range(8):
+    print(n+1)
+for z in range(1,2001,5):
+    print(z)
+for x in range (1,12,3):
+    print(x+8)
