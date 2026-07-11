bba848b3913c4a63aaa71704cb147963b57bba40
Aviral Tyagi <aviraltyagi.mrt@gmail.com>
2026-07-11 18:41:14 +0530

    Functions Done

---
+++ Functions.py
@@ -0,0 +1,25 @@
+def isgreater(a,b):
+    if (a>b):
+        print(f"{a} is greater than {b}")
+    else :
+        print(f"{b} is greater than {a}")
+a = 78
+b = 90
+isgreater(a,b)
+
+def issmaller(a,b):
+    if (a<b):
+        print(f"{a} is smaller than {b}")
+    else :
+        print(f"{b} is lesser than {a}")
+c = 76
+d = 75
+issmaller(c,d)
+
+def Hello(a,b,c,d):
+    print(a+b*c/d)
+
+Hello(25,2,3,4)
+
+def Will_Create_it_Soon(a,b,c):
+    pass # The pass function Keep the functions as it is and you can make changes whenever you want 
