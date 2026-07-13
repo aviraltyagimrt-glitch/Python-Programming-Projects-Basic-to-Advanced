3bf2193927701da2faa47ad6ff734e6647a9373c
Aviral Tyagi <aviraltyagi.mrt@gmail.com>
2026-07-13 14:54:27 +0530

    Functions Done

--- Functions.py
+++ Functions.py
@@ -23,3 +23,20 @@ Hello(25,2,3,4)
 
 def Will_Create_it_Soon(a,b,c):
     pass # The pass function Keep the functions as it is and you can make changes whenever you want 
+
+def names(Fname , Mname = "Weds" , Lname = "Gaurika" ):
+    print("A very special Invitation" , Fname , Mname , Lname)
+names("AVIRAL",Lname = "GAURIKA")
+
+def average(*numbers):
+    sum = 0
+    for i in numbers:
+        sum = sum + i
+    return(f"The average is {(sum/len(numbers))}")
+
+z = average(2,3,4,5,6)
+print(z)
+
+def copy(**names):
+    print("Hello" , names["Fname"] , names["Mname"] , names["Lname"])
+copy(Fname = "Gaurika" , Mname = "Weds" , Lname = "Aviral")
