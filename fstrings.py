72e7d410a83277083ca81bf3b5148b4e43f76a06
Aviral Tyagi <aviraltyagi.mrt@gmail.com>
2026-07-21 08:32:31 +0530

    f strings Done

---
+++ fstrings.py
@@ -0,0 +1,13 @@
+a = 74
+b = 65
+print(f"The addition of {a} and {b} is {a+b}")
+print(f"Hello Everyone my name is {{Aviral}} and I study in {{College}}") # To consider braces while using f strings
+print("Hello Everyone my name is {Aviral} and I study in {College}") # Without using f strings
+c = "Tyagi ji"
+print(f"Hii Everybody my First name is Aviral and Surname is {c}")
+d = [1,2,"VS Code","DSA","!"]
+print(f"This is how to print a list using fstrings {d}")
+e = (1,2,"VS Code","DSA","!")
+print(f"This is for a Tuple {e}")
+f = {1:"Aviral",2:"Tyagi",3:"Student","College":"KCC ITM"}
+print(f"This is for a Dictionary {f}")
