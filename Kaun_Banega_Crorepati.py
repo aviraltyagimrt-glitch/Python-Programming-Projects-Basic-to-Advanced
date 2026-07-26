a6403049a4fe0d810c13b2baefbf7a4f2aaea00a
Aviral Tyagi <aviraltyagi.mrt@gmail.com>
2026-07-26 21:13:22 +0530

    Folder Changed

---
+++ Kaun_Banega_Crorepati.py
@@ -0,0 +1,13 @@
+print("Welcome to the Kaun Banega Crorepati Game")
+a = input("Please enter your name before playing\n")
+print(f"Great job {a} now , before entering the game follow these instructions")
+print("There will be 11 question at all")
+print("For Each question you have to choose answer from the 4 options mentioned below")
+print("You have 3 lifelines in total and after their usage no clue will be provided")
+print("The respected amount will be credited in your balance as per the correction rate")
+b = input("Are you Ready? (Y/N)\n")
+if(b == "Y" or b == "y"):
+    print("Let's go")
+    L1 = ["When was First AI app Introduced ?","Name the latest Chip NVIDIA has launched","Name the latest party formed by the youth for the"]
+
+ 
