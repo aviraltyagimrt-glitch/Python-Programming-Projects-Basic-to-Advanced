a6fe8fe26213485a8fe3c2208f1432ef0efea810
Aviral Tyagi <aviraltyagi.mrt@gmail.com>
2026-07-28 21:34:32 +0530

    Choices Added

--- Code_Decode.py
+++ Code_Decode.py
@@ -1,5 +1,7 @@
 # Welcome to the game of coding and decoding
 #Coding
+name = input("Kindly Enter your Name\n")
+ask = int(input("Press 1 to code and 0 to decode\n"))
 import random
 alphabets1 = "abcdefghijklmnopqrstuvwxyz"
 alphabets2 = "abcdefghijklmnopqrstuvwxyz"
@@ -14,41 +16,42 @@ a4 = random.choice(alphabets4)
 a5 = random.choice(alphabets5)
 a6 = random.choice(alphabets6)
 string = input("Enter your message\n")
-if (len(string) > 2):
-    l = list(string)
-    n = l.pop(0)
-    z = l.append(n)
-    x1 = l.append(a1)
-    x2 = l.append(a2)
-    x3 = l.append(a3)
-    x4 = l.insert(0,a4)
-    x5 = l.insert(1,a5)
-    x6 = l.insert(2,a6)
-    str1 = "".join(l)
-    print(str1)
+if (ask == 1):  
+    if (len(string) > 2):
+        l = list(string)
+        n = l.pop(0)
+        z = l.append(n)
+        x1 = l.append(a1)
+        x2 = l.append(a2)
+        x3 = l.append(a3)
+        x4 = l.insert(0,a4)
+        x5 = l.insert(1,a5)
+        x6 = l.insert(2,a6)
+        str1 = "".join(l)
+        print(str1)
+
+    else:
+        reversed_string = string[::-1]
+        print(reversed_string)
+elif (ask == 0):    
+# Decoding
+    if (len(string) > 2):
+        decoded_list = list(string)
+        removed_elements = decoded_list[3:-3]
+        #print(removed_elements)
+        popped_item = removed_elements.pop()
+        #print(popped_item)
+        #print(removed_elements)
+        inserted_item = removed_elements.insert(0,popped_item)
+        #print(removed_elements)
+        new_string = "".join(removed_elements)
+        print(new_string)
+
+    elif (len(string) <=2 ):
+        decoded_list2 = list(string)
+        reversed_list = decoded_list2[::-1]
+        print("".join(reversed_list))
 
 else:
-    reversed_string = string[::-1]
-    print(reversed_string)
-
-# Decoding
-if (len(string) > 2):
-    decoded_list = list(str1)
-    removed_elements = decoded_list[3:-3]
-    #print(removed_elements)
-    popped_item = removed_elements.pop()
-    #print(popped_item)
-    #print(removed_elements)
-    inserted_item = removed_elements.insert(0,popped_item)
-    #print(removed_elements)
-    new_string = "".join(removed_elements)
-    print(new_string)
-
-# elif (len(string) <=2 ):
-#     decoded_list2 = list(string)
-#     reversed_list = decoded_list2[::-1]
-#     print("".join(reversed_list))
-
-# else:
-#     print("Enter Valid String format")
+    print("Enter Valid String format")
 
