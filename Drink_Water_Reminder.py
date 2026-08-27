2504f9eb31f6f5138e43bae91c4c3e5f08e70d3f
Aviral Tyagi <aviraltyagi.mrt@gmail.com>
2026-08-27 12:29:54 +0530

    Drink Water Reminder Done

--- Drink_Water_Reminder.py
+++ Drink_Water_Reminder.py
@@ -1,11 +1,18 @@
-import win32api
-import time
-import win32com.client
-names = ["Aviral"]
-speaker = win32com.client.Dispatch("SAPI.SpVoice")
-for i in names:
-    time.sleep(7)
-    reminder = f"This a reminder to drink a glass of water {i}"
-    print(f"This a Reminder to drink a glass of water {i}")
-    speaker.Speak(reminder)
+# import win32api
+# import win32com.client
+# names = ["Aviral"]
+# speaker = win32com.client.Dispatch("SAPI.SpVoice")
+# for i in names:
+#     time.sleep(7)
+#     reminder = f"This a reminder to drink a glass of water {i}"
+#     print(f"This a Reminder to drink a glass of water {i}")
+#     speaker.Speak(reminder)
 
+import time
+from plyer import notification
+name = input("Enter your name\n")
+i = 1
+while (i<1):
+    time.sleep(3600)
+    notification.notify(title = "Reminder",message = f"This is a reminder to drink water {name}",timeout=4)
+    i = i+1
